0fde0c44314b1a6b02adb57af3c71166d371fe44
FreedomWriter <natalienetworks@gmail.com>
2020-02-17 16:32:04 -0600

    updated restricted middleware

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -5,14 +5,12 @@ module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
-    jwt.verify(token, secrets.jwt, (err, payload) => {
+    jwt.verify(token, process.env.JWT_SECRETS, (err, payload) => {
       if (err) {
-        res
-          .status(403)
-          .json({
-            message: "You are not authorized FROM RESTIRCTED MIDDLEWARE",
-            error: err.message
-          });
+        res.status(403).json({
+          message: "You are not authorized FROM RESTIRCTED MIDDLEWARE",
+          error: err.message
+        });
       } else {
         req.userId = payload.userId;
         next();
