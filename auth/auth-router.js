802cf46157914f71388c5711ad1f398db4573327
FreedomWriter <natalienetworks@gmail.com>
2020-02-17 21:50:14 -0600

    deleted unsed import

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const secrets = require("../config/secrets");
 const validator = require("../middleware/validator");
 
 const usersModel = require("../users/users-model.js");
