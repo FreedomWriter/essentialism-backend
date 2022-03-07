320ad77363626b0b12e5fe5c37bf2bd95fbefda0
FreedomWriter <natalienetworks@gmail.com>
2022-03-07 09:19:21 -0600

    env vars

--- data/db.config.js
+++ data/db.config.js
@@ -2,6 +2,7 @@ const knex = require("knex");
 const config = require("../knexfile");
 
 const env = process.env.DB_ENV || "development";
+console.log(process.env.NODE_ENV);
 
 const db = knex(config[env]);
 
