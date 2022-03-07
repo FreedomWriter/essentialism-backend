9e1a3b1cf8cedc2faad601eeb520dd3b5fab8b0b
FreedomWriter <natalienetworks@gmail.com>
2022-03-07 09:42:23 -0600

    migrate

--- data/db.config.js
+++ data/db.config.js
@@ -2,7 +2,7 @@ const knex = require("knex");
 const config = require("../knexfile");
 
 const env = process.env.DB_ENV || "development";
-console.log(process.env.NODE_ENV);
+console.log(process.env.DB_ENV);
 
 const db = knex(config[env]);
 
